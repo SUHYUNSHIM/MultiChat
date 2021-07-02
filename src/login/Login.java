a161889fd58f4322be4f6086e8b0d66325fcf4c2
SUHYUNSHIM <lisa9963suhyun@gmail.com>
2021-07-02 21:16:31 +0900

    serverclass+

--- src/login/Login.java
+++ src/login/Login.java
@@ -7,14 +7,17 @@ import javax.swing.JFrame;
 import javax.swing.JPanel;
 import javax.swing.border.EmptyBorder;
 
+import ChatFrame.chatMain;
 import JDBCInfo.Member;
 import JDBCInfo.MemberDao;
 import JDBCInfo.MemberDaoImpl;
+import chatSC.ServerClass;
 
 import javax.swing.JLabel;
 import javax.swing.JTextField;
 import javax.swing.JButton;
 import java.awt.event.ActionListener;
+import java.io.IOException;
 import java.awt.event.ActionEvent;
 
 public class Login extends JFrame {
@@ -76,7 +79,7 @@ public class Login extends JFrame {
 		lblLoginBanner.setBounds(72, 46, 260, 18);
 		contentPane.add(lblLoginBanner);
 		
-		JButton btnSave = new JButton("가입완료"); //데이터베이스에 회원 정보를 넘겨주어야 한다. 
+		JButton btnSave = new JButton("가입"); //데이터베이스에 회원 정보를 넘겨주어야 한다. 
 		btnSave.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent arg0) {
 				MemberDao memberDao = new MemberDaoImpl();
@@ -87,16 +90,31 @@ public class Login extends JFrame {
 				//System.out.println("해당 구에 거주하는 유저의 닉네임은 : " +member.getNickName());
 				Member member = memberDao.getMemberByRegion("동작구"); //나중에 스캐너든, inputdialog 든 값을 입력받아서 여기에 넣을 것이다.
 				System.out.println("해당 구에 거주하는 유저의 닉네임은 "+member.getNickName());
+				System.out.println("가입이 완료되었습니다.");
+				
 			}
 		});
 		btnSave.setBounds(72, 349, 105, 27);
 		contentPane.add(btnSave);
 		
 		///------------------------------------------------------------//
-		JButton btnChatStart = new JButton("채팅 시작하기");
+		JButton btnChatStart = new JButton("채팅하러 가기");
 		btnChatStart.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent e) {
 				//ip와 port를 입력하는 창으로 넘어간다. 
+				//이 버튼을 누르면 서버가 동작하는 것은 어떨까
+				dispose();
+				setVisible(false); 
+				new chatMain().setVisible(true);
+				System.out.println("채팅 메인 화면으로 전환 성공.");
+				
+			/*	try {
+					new ServerClass(12700); //미리 포트번호를 넣어두고 버튼 누를 시 실행되도록 하였음.
+					System.out.println("서버 동작 중");
+				} catch (IOException e1) {					
+					e1.printStackTrace();
+				}*/
+				
 			}
 		});
 		btnChatStart.setBounds(191, 349, 141, 27);
