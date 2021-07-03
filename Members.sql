3ade89e9dc9d75b25d2157482513d2825bef2b46
SUHYUNSHIM <lisa9963suhyun@gmail.com>
2021-07-04 00:38:29 +0900

    sql 수정

--- Members.sql
+++ Members.sql
@@ -4,4 +4,4 @@ update member
 set birthday = '20000423'
 where region = 'µø¿€±∏';
 
-select count(*) from all_tables where table_name = 'member';
+select count(*) from all_tables where table_name = 'member';   
